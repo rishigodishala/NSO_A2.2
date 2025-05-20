8fae895f4a8f6ecdff322faaa1effd54b48045fd
GODISHALA RISHI <godishalarishi@host-193-11-187-227.eduroam.bth.se>
2025-05-20 12:32:58 +0200

    final

--- application2.py
+++ application2.py
@@ -1,13 +1,19 @@
-import flask
+# files/application2.py
+
+from flask import Flask
 import time
 import socket
 
 h_name = socket.gethostname()
-IP_addres = socket.gethostbyname(h_name)
+IP_address = socket.gethostbyname(h_name)
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    Time= time.strftime("%H:%M:%S")
-    return Time+" Serving from "+h_name+" ("+IP_addres+")\n"
+    current_time = time.strftime("%H:%M:%S")
+    return current_time + " Serving from " + h_name + " (" + IP_address + ")\n"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
